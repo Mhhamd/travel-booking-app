243b31679e4da3e82b18ccdc3eaf4a371e3e2bd0
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-02-28 15:28:12 +0200

    feat: fetching popular destinations with related images

--- src/components/TopDestinations/TopDestinations.tsx
+++ src/components/TopDestinations/TopDestinations.tsx
@@ -1,85 +1,97 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import mockData from '../../data/MOCK_DATA.json?raw';
+import { dataTypes } from '../../types/flightType';
+
+interface UnsplashImage {
+    urls: {
+        full: string;
+        small: string;
+    };
+}
+
+interface Destination {
+    city: string;
+    image: UnsplashImage | null;
+}
+
 function TopDestinations() {
+    const [destinations, setDestinations] = useState<Destination[]>([]);
+    const data = JSON.parse(mockData);
+    const filter = data.filter((item: dataTypes) => {
+        return item.arrival_country_popular === 'Popular';
+    });
+
+    const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+
+    const uniqueDestinations: typeof filter = [];
+    const seenCities = new Set();
+    for (const item of filter) {
+        if (!seenCities.has(item.arrival_city)) {
+            seenCities.add(item.arrival_city);
+            uniqueDestinations.push(item);
+        }
+    }
+
+    const randomSelection = uniqueDestinations
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 5);
+
+    async function fetchImages(query: string) {
+        try {
+            const response = await fetch(
+                `https://api.unsplash.com/search/photos?query=${query}&orientation=landscape&client_id=${accessKey}&per_page=1`
+            );
+            const data = await response.json();
+            return data.results?.[0] || null;
+        } catch (error) {
+            console.error('error fetching similar images', error);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        async function fetchAndSetImages() {
+            const results: Destination[] = await Promise.all(
+                randomSelection.map(async (item: dataTypes) => {
+                    const image = await fetchImages(item.arrival_city);
+                    return { city: item.arrival_city, image };
+                })
+            );
+            setDestinations(results);
+        }
+        fetchAndSetImages();
+    }, []);
     return (
         <div className="flex-center gap-12 flex-col p-12 mt-30">
-            <div className=" flex-center flex-col gap-4">
-                <h1 className="font-medium  text-[#f96c50] tracking-wider">
+            <div className="flex-center flex-col gap-4">
+                <h1 className="font-medium text-[#f96c50] tracking-wider">
                     Where to Go
                 </h1>
                 <h1 className="text-gray-700 font-bold tracking-widest text-3xl">
                     Explore Exotic Destinations
                 </h1>
             </div>
-            <div className="grid grid-cols-4 gap-x-4 gap-y-4 place-content-center capitalize  ">
-                <div className="relative w-80 rounded-xl overflow-hidden  ">
-                    <img
-                        className="object-center object-cover w-full h-full img-hover"
-                        src="/assets/morocco.jpg"
-                        alt="morroco"
-                        loading="lazy"
-                    />
-                    <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
-                        Morocco
-                    </h1>
-                    <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
-                        Popular
-                    </span>
-                </div>
-                <div className="relative w-80 rounded-xl overflow-hidden ">
-                    <img
-                        className="object-center object-cover w-full h-full img-hover"
-                        src="/assets/unitedkingdom.jpg"
-                        alt="morroco"
-                        loading="lazy"
-                    />
-                    <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
-                        United kingdom
-                    </h1>
-                    <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
-                        Popular{' '}
-                    </span>
-                </div>
-                <div className="relative w-80 rounded-xl overflow-hidden ">
-                    <img
-                        className="object-center object-cover w-full h-full img-hover"
-                        src="/assets/singapore.jpg"
-                        loading="lazy"
-                        alt="morroco"
-                    />
-                    <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
-                        singapore
-                    </h1>
-                    <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
-                        Popular{' '}
-                    </span>
-                </div>
-                <div className="relative w-80 rounded-xl overflow-hidden ">
-                    <img
-                        className="object-center object-cover w-full h-full img-hover"
-                        src="/assets/thailand.jpg"
-                        alt="morroco"
-                        loading="lazy"
-                    />
-                    <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
-                        thailand
-                    </h1>
-                    <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
-                        Popular{' '}
-                    </span>
-                </div>
-                <div className="relative w-80 rounded-xl overflow-hidden ">
-                    <img
-                        className="object-center object-cover w-full h-full img-hover"
-                        src="/assets/italy.jpg"
-                        alt="morroco"
-                        loading="lazy"
-                    />
-                    <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
-                        Italy
-                    </h1>
-                    <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
-                        Popular{' '}
-                    </span>
-                </div>
+            <div className="grid grid-cols-4 gap-x-4 gap-y-4 place-content-center capitalize">
+                {destinations.map(({ city, image }, index) => (
+                    <div
+                        key={index}
+                        className="relative w-80 rounded-xl overflow-hidden"
+                    >
+                        <img
+                            className="object-center object-cover w-full h-full img-hover"
+                            src={image?.urls.small || '/assets/placeholder.jpg'}
+                            alt={city}
+                            loading="lazy"
+                        />
+                        <h1 className="absolute bottom-0 text-white text-2xl font-bold tracking-wider p-6">
+                            {city}
+                        </h1>
+                        <span className="absolute top-0 right-0 rounded-lg font-semibold bg-[#e06149] text-white m-6 py-2 px-6">
+                            Popular
+                        </span>
+                    </div>
+                ))}
             </div>
         </div>
     );
---
+++ src/types/flightType.ts
@@ -0,0 +1,15 @@
+export interface dataTypes {
+    airline: string;
+    arrival_airport: string;
+    arrival_city: string;
+    arrival_country: string;
+    arrival_country_popular: string;
+    class: string;
+    departure_airport: string;
+    departure_city: string;
+    departure_country: string;
+    duration: number;
+    flight_number: string;
+    price: number;
+    seats_available: number;
+}
