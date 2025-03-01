b3ddac44f1b0c48336b9eb9f6fc15f8735256c26
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-03-01 22:29:23 +0200

    feat added: flight details

--- src/components/IconicTravelSpots/IconicTravelSpots.tsx
+++ src/components/IconicTravelSpots/IconicTravelSpots.tsx
@@ -22,6 +22,8 @@ interface Destination {
     departureCity: string;
     arrivalCountry: string;
     departureCountry: string;
+    duration: number;
+    seatsAvailable: number;
     image: UnsplashImage | null;
 }
 
@@ -53,6 +55,8 @@ function IconicTravelSpots() {
                         arrivalCountry: item.arrival_country,
                         departureCity: item.departure_city,
                         departureCountry: item.departure_country,
+                        duration: item.duration,
+                        seatsAvailable: item.seats_available,
                         image,
                     };
                 })
@@ -61,6 +65,10 @@ function IconicTravelSpots() {
         }
         fetchAndSetImages();
     }, []);
+    const randomPrice = (min = 100, max = 300) => {
+        return (Math.random() * (max - min) + min).toFixed();
+    };
+
     return (
         <div className="flex-center w-full flex-col gap-12 p-12">
             <div className="flex-center gap-4 flex-col">
@@ -102,42 +110,54 @@ function IconicTravelSpots() {
                                     </p>
                                 </div>
                                 <div className="border w-full flex-start gap-6">
-                                    <div className="p-4 pt-0 bg-white border border-black/20 rounded-md left-0 absolute -bottom-41 w-[100%] shadow-lg">
-                                        <div className="flex items-center justify-start">
+                                    <div className="p-4 bg-white border border-black/20 rounded-md left-0 absolute -bottom-41 w-[100%] shadow-lg">
+                                        {/* <div className="flex items-center justify-start">
                                             <img
                                                 className="w-24"
                                                 src="/assets/rating.png"
                                                 alt="Rating"
                                             />
                                             <p className="text-gray-400">4.6</p>
-                                        </div>
-                                        <p className="text-gray-700 text-center font-bold tracking-wider">
+                                        </div> */}
+                                        <p className="text-gray-700 text-left font-bold tracking-wider">
                                             {destination.arrivalCity} (
                                             {destination.arrivalCountry})
                                         </p>
-                                        <p className="text-[#ff6b6b] flex-start mt-4 gap-1 flex items-center justify-center">
+                                        <p className="text-[#ff6b6b] flex-start mt-4 gap-1 ">
                                             <IoLocation />
-                                            <span className="text-gray-400">
-                                                Central Park West NY, USA
-                                            </span>
+                                            <div className="flex-start flex-col">
+                                                <span className="text-gray-400 ">
+                                                    Departure:{' '}
+                                                    {destination.departureCity},{' '}
+                                                    {
+                                                        destination.departureCountry
+                                                    }{' '}
+                                                </span>
+                                                <span className="text-gray-400 ">
+                                                    Arrival:{' '}
+                                                    {destination.arrivalCity},{' '}
+                                                    {destination.arrivalCountry}
+                                                </span>
+                                            </div>
                                         </p>
                                         <p className="text-gray-400 mt-2 tracking-wider">
                                             From{' '}
                                             <span className="text-[#ff6b6b]">
-                                                $59.00
+                                                ${randomPrice()}
                                             </span>
                                         </p>
                                         <div className="bg-gray-200 mt-4 p-2 flex-between">
                                             <p className="flex-center font-extrabold gap-2 text-sm text-[#ff6b6b]">
                                                 <CiClock1 />{' '}
                                                 <span className="text-gray-700 font-medium">
-                                                    10 days
+                                                    {destination.duration}h
                                                 </span>
                                             </p>
                                             <p className="flex-center font-extrabold gap-2 text-sm text-[#ff6b6b]">
                                                 <MdGroup />{' '}
                                                 <span className="text-gray-700 font-medium">
-                                                    50
+                                                    {destination.seatsAvailable}{' '}
+                                                    Seats Left
                                                 </span>
                                             </p>
                                             <p className="flex-center font-bold gap-2 text-sm btn-hover cursor-pointer transition-all duration-500 text-[#ff6b6b]">
