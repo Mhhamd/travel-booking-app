c192cf2f8b97f208df04e21f7ec963521a45c2e0
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-04-11 08:47:25 +0200

    Fix github pages

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
         <SmoothScroll>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
-                    <HashRouter basename="/travel-booking-app">
+                    <HashRouter basename="/travel-booking-app/">
                         <RouterProvider router={router} />
                     </HashRouter>
                 </PersistGate>
