7684073004fe5ca987de180e24c2959fc6ee3946
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-04-11 08:49:54 +0200

    Fix github pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import SmoothScroll from './utils/SmoothScroll.tsx';
-import { HashRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { persistor } from './state/store.ts';
 import { router } from './routes/routes.tsx';
@@ -13,9 +13,9 @@ createRoot(document.getElementById('root')!).render(
         <SmoothScroll>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
-                    <HashRouter basename="/travel-booking-app/">
+                    <BrowserRouter basename="/travel-booking-app">
                         <RouterProvider router={router} />
-                    </HashRouter>
+                    </BrowserRouter>
                 </PersistGate>
             </Provider>
         </SmoothScroll>
